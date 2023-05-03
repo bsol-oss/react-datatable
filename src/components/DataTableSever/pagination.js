bc91b2ca423b2b0bb673f6a55a6606562ca8940c
jyotipadhi1 <jyotipadhi1@gmail.com>
2023-05-03 20:53:27 +0530

    pagination + header change according to new requirement

--- src/components/DataTableSever/pagination.js
+++ src/components/DataTableSever/pagination.js
@@ -27,104 +27,88 @@ const Pagination = ({
                 justifyContent={'space-between'}
                 color={defaultValues.colorSet1.serverText}
                 alignItems={'center'}
+                fontSize={'0.85rem'}
             >
                 <div>
-                    {/* <ButtonGroup gap="1" marginRight={'10px'}>
-                <Button
-                    onClick={() => gotoPage(0)}
-                    isDisabled={!canPreviousPage}
-                    colorScheme="gray"
-                    size="sm"
-                >
-                    {'<<'}
-                </Button>{' '}
-                <Button
-                    onClick={() => previousPage()}
-                    isDisabled={!canPreviousPage}
-                    colorScheme="gray"
-                    size="sm"
-                >
-                    {'<'}
-                </Button>{' '}
-                <Button
-                    onClick={() => nextPage()}
-                    isDisabled={!canNextPage}
-                    colorScheme="gray"
-                    size="sm"
-                >
-                    {'>'}
-                </Button>{' '}
-                <Button
-                    onClick={() => gotoPage(pageCount - 1)}
-                    isDisabled={!canNextPage}
-                    colorScheme="gray"
-                    size="sm"
-                >
-                    {'>>'}
-                </Button>{' '}
-            </ButtonGroup> */}
                     {t('Showing')} {start < totalCount ? start : totalCount}{' '}
                     {t('to')} {end < totalCount ? end : totalCount} {t('of')}{' '}
                     {totalCount} {t('results')}
                 </div>
+
+                <div>
+                    Go to page:{' '}
+                    <Input
+                        type="number"
+                        value={pageIndex + 1}
+                        onChange={e => {
+                            const page = e.target.value
+                                ? Number(e.target.value) - 1
+                                : 0
+                            gotoPage(page)
+                        }}
+                        width="70px"
+                        marginRight="10px"
+                        size="sm"
+                    />
+                </div>
+                <div>
+                    <Select
+                        value={pageSize}
+                        onChange={e => {
+                            setPageSize(Number(e.target.value))
+                        }}
+                        style={{
+                            border: '1px solid black',
+                            fontSize: '12px',
+                            borderColor: '#e2e8f0',
+                        }}
+                        width={'120px'}
+                        size="sm"
+                    >
+                        {pageSizes.map(pageSize => (
+                            <option key={pageSize} value={pageSize}>
+                                Show {pageSize}
+                            </option>
+                        ))}
+                    </Select>
+                </div>
                 <div>
                     <ButtonGroup gap="1" marginRight={'10px'}>
+                        <Button
+                            onClick={() => gotoPage(0)}
+                            isDisabled={!canPreviousPage}
+                            colorScheme="gray"
+                            size="sm"
+                        >
+                            {'<<'}
+                        </Button>{' '}
                         <Button
                             onClick={() => previousPage()}
                             isDisabled={!canPreviousPage}
                             colorScheme="gray"
                             size="sm"
-                            variant="outline"
-                            width={['70px', '90px']}
-                            fontSize={['12px', '14px']}
                         >
-                            {t('Previous')}
+                            {'<'}
                         </Button>{' '}
                         <Button
                             onClick={() => nextPage()}
                             isDisabled={!canNextPage}
                             colorScheme="gray"
                             size="sm"
-                            variant="outline"
-                            width={['60px', '80px']}
-                            fontSize={['12px', '14px']}
                         >
-                            {t('Next')}
-                        </Button>
+                            {'>'}
+                        </Button>{' '}
+                        <Button
+                            onClick={() => gotoPage(pageCount - 1)}
+                            isDisabled={!canNextPage}
+                            colorScheme="gray"
+                            size="sm"
+                        >
+                            {'>>'}
+                        </Button>{' '}
                     </ButtonGroup>
                 </div>
             </Flex>
-
-            {/* <span>
-            | Go to page:{' '}
-            <Input
-                type="number"
-                value={pageIndex + 1}
-                onChange={e => {
-                    const page = e.target.value
-                        ? Number(e.target.value) - 1
-                        : 0
-                    gotoPage(page)
-                }}
-                width="100px"
-                marginRight="10px"
-            />
-        </span>
-        <div>
-            <Select
-                value={pageSize}
-                onChange={e => {
-                    setPageSize(Number(e.target.value))
-                }}
-                style={{ border: '1px solid black' }}
-            >
-                {pageSizes.map(pageSize => (
-                    <option key={pageSize} value={pageSize}>
-                        Show {pageSize}
-                    </option>
-                ))}
-            </Select>
-        </div> */}
         </PaginationDiv>
     )
 }
